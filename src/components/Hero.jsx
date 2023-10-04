8ac3e58a3fd8f35b5b3704a6776dd0d3d7049784
Nahid Hasan <nahidhasan1001@gmail.com>
2023-10-05 04:59:47 +0600

    update localhost to env

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/hero", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hero`, {
     cache: "no-store",
   });
   if (!res.ok) {
