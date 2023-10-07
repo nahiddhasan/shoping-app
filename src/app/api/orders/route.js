6d30a7db823357f52608513091d6130f5812b307
Nahid Hasan <nahidhasan1001@gmail.com>
2023-10-07 19:44:18 +0600

    orders prblm

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -30,7 +30,6 @@ export const POST = async (NextRequest) => {
 //Fetching Orders
 export const GET = async (req) => {
   const session = await getAuthSession();
-  console.log(session)
   if (session) {
     try {
       if (session.user.role === "ADMIN") {
--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,13 +1,23 @@
-import { PrismaClient } from '@prisma/client'
-let prisma
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+// import { PrismaClient } from '@prisma/client'
+// let prisma
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
+// }
 
-export default prisma
+// export default prisma
 
+
+
+import { PrismaClient } from "@prisma/client";
+
+
+
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
